b69f37f9a04aa3a1680b8e81c510155c9e6cc43d
Your Name <you@example.com>
2024-06-18 09:32:44 +0000

    2024-06-18

---
+++ pip-install.sh
@@ -0,0 +1,11 @@
+pip install transformers accelerate sentencepiece
+pip install bitsandbytes
+pip install langchain langchain_community
+pip install intel-extension-for-transformers
+pip install datasets
+pip install sentence-transformers
+pip install chromadb
+pip install selenium
+pip install unstructured
+pip install pymupdf
+pip install spacy
